1f83042b3f582d0b328dc82b38e05c00a8e0353b
Anuj Sharma <19ucc109@lnmiit.ac.in>
2019-10-31 08:48:31 -0700

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,46 @@
+let index = 1;
+let i = 0;
+function sub() {
+  let input = document.querySelector(".input");
+  let p = [];
+  if (input.value != "") {
+    p[index] = input.value;
+    input.value = "";
+    console.log(index, p[index]);
+    let items = document.createElement("div");
+    let done = document.createElement("div");
+    done.className = "tick";
+    items.appendChild(done);
+    let itemsText = document.createElement("div");
+    itemsText.className = "item-text";
+    itemsText.innerHTML = p[index];
+    items.appendChild(itemsText);
+    items.className = "teth";
+    items.id = index;
+// done line
+    items.onclick = function() {
+        if(i==0)
+        {
+        done.style.cssText =
+        " transition: 0.3s; animation:line-on 0.9s; height: 2px; width: 400px; margin-left:2vw; border-radius: 0%;";
+        }
+    }
+    let cancel = document.createElement("div");
+    cancel.className = "cross";
+    cancel.innerHTML = '<img src="../img/close.png" id="close-icon" />';
+// close item 
+    cancel.onclick = function() {
+      cancel.parentElement.remove();
+    };
+    items.appendChild(cancel);
+    document.querySelector(".two").appendChild(items);
+    input.focus();
+    index++;
+  }
+}
+
+document.onkeydown = function(e){
+    if (e.keyCode === 13) {
+        sub();
+    }
+  };
